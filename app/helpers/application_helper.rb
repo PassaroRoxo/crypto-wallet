3cfcc95c7d72289897e5edd15ac785551f9b01f7
Artur <henriqueacg@gmail.com>
2019-12-31 15:57:21 -0300

    Ambientes, var de instancia, partials, fluxo MVC

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
     def dateBR(dateUS)
         dateUS.strftime("%d/%m/%Y")
     end
+
+    def railsEnv
+        if Rails.env.development?
+            "Desenvolvimento"
+        elsif Rails.env.production?
+            "Produção"
+        else
+            "Teste"
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Coin.create!(
+    decription: "Bitcoin",
+    acronym: "BTC",
+    url_image: "https://img2.gratispng.com/20180409/xgq/kisspng-bitcoin-cash-virtual-currency-litecoin-bitcoin-5acbf6b0841631.5110736115233164005411.jpg"
+)
